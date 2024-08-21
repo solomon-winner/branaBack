77f443f9a6930db6211863b1ca8e5d995605c58f
solomon-winner <solyalew3@gmail.com>
2024-08-21 15:55:15 +0300

    ...

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
+const app = express();
+const port  = 5000;
+
+app.use(bodyParser.json());
