179eff72356d74284aaab95e9c4fd39da74a7db6
solomon-winner <solyalew3@gmail.com>
2024-08-22 14:25:49 +0300

    ...categories models...

--- models/categories.js
+++ models/categories.js
@@ -1,7 +1,14 @@
 import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema({
-    
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
 });
 
 export const Cateory = mongoose.model("Category", categorySchema);
