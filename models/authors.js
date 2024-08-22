68a969647847d7a46969717de0a1c6671bacdeb2
solomon-winner <solyalew3@gmail.com>
2024-08-22 14:18:54 +0300

    ...book models...

--- models/authors.js
+++ models/authors.js
@@ -1,7 +1,26 @@
 import mongoose from "mongoose";
 
 const authorSchema = new mongoose.Schema({
-    
+    name: {
+        type: String,
+        required: true,
+    },
+    img: {
+        type: String,
+        required: true,
+    },
+    bio: {
+        type: String,
+        required: true,
+    },
+    birthDate: {
+        type: Date,
+        required: true,
+    },
+    deathDate: {
+        type: Date,
+        required: false,
+    },
 });
 
 export const Author = mongoose.model("Author", authorSchema);
