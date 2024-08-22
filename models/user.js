43612c3b3dae6baa1ec768378c22f45ba00e2d50
solomon-winner <solyalew3@gmail.com>
2024-08-22 12:43:55 +0300

    ...User models...

--- models/user.js
+++ models/user.js
@@ -1,5 +1,14 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type : String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    }
+});
 
 export const User = mongoose.model('User', userSchema);
