e63f8d0f5189f5eb34e026254bcc3c57498abe89
solomon-winner <solyalew3@gmail.com>
2024-08-22 13:00:03 +0300

    ...Bank account models...

---
+++ models/bankAccount.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const BankAccountSchema = new mongoose.Schema({
+    bankAccountName: {
+        type : String,
+        required: true
+    },
+    bankAccountNo: {
+        type: String,
+        required: true
+    },
+    });
+
+export const BankAccount = mongoose.model('User', userSchema);
