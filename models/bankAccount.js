9db8f57495ba32bf7dbb4f6bf16c5211e1c25081
solomon-winner <solyalew3@gmail.com>
2024-08-22 16:04:18 +0300

    ...modify models...

--- models/bankAccount.js
+++ models/bankAccount.js
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose';
 
 const BankAccountSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
     bankAccountName: {
         type : String,
         required: true
