233ef2c9427a3be23f4f88ef40babad07088c5b1
solomon-winner <solyalew3@gmail.com>
2024-08-22 14:52:34 +0300

    ...shelve models...

--- models/shelve.js
+++ models/shelve.js
@@ -1,4 +1,17 @@
 import mongoose from "mongoose";
 
-const shelveSchema = new mongoose.Schema({});
+const shelveSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    bookCount: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+});
 export const Shelve = mongoose.model("Shelve", shelveSchema);
