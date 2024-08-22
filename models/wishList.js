375f1a433ee93eb16082e2987a6c50fd2ae233bf
solomon-winner <solyalew3@gmail.com>
2024-08-22 14:56:19 +0300

    ...wishList  models...

--- models/wishList.js
+++ models/wishList.js
@@ -1,5 +1,22 @@
 import mongoose from "mongoose";
 
-const wishListSchema = new mongoose.Schema({});
+const wishListSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    bookCount: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    isGift: {
+        type: Boolean,
+        default: false,
+    },
+});
 
 export const WishList = mongoose.model("WishList", wishListSchema);
