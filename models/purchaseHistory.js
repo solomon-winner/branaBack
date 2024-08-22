e6e6efe939031a051b9c80b0ef635459ef176273
solomon-winner <solyalew3@gmail.com>
2024-08-22 14:48:49 +0300

    ...purchase history models...

--- models/purchaseHistory.js
+++ models/purchaseHistory.js
@@ -1,5 +1,48 @@
 import mongoose from "mongoose";
+import { BankAccountSchema } from "./bankAccount";
+import { AddressSchema } from "./address"; // Assuming AddressSchema is defined in address.js
 
-const purchseHistorySchema = new mongoose.Schema({});
+const purchaseHistorySchema = new mongoose.Schema({
+    book: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Book",
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    totalAmount: {
+        type: Number,
+        required: true,
+    },
+    paymentMethod: {
+        type: String,
+        required: true,
+    },
+    deliveryAddress: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Address",
+        required: true,
+    },
+    bankAccount: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "BankAccount",
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+    },
+});
 
-export const PurchaseHitory = mongoose.model("PurchaseHistory", purchseHistorySchema);
+export const PurchaseHistory = mongoose.model("PurchaseHistory", purchaseHistorySchema);
