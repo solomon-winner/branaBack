a6576840b7fbac82419d3cd85df6bac4286d003f
solomon-winner <solyalew3@gmail.com>
2024-08-23 10:52:51 +0300

    server...

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port  = 5000;
