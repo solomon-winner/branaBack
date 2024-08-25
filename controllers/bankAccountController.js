69f989608e3bd68405328b10010f9f4deb7be3f6
solomon-winner <solyalew3@gmail.com>
2024-08-25 16:00:45 +0300

    Account Controllers...

---
+++ controllers/bankAccountController.js
@@ -0,0 +1,4 @@
+export const addAccount = (req, res) => {};
+export const updateAccount = (req, res) => {};
+export const getAccounts = (req, res) => {};
+export const removeAccount = (req, res) => {};
