4f124142447914de1fd62ff149a97407938ecc2b
solomon-winner <solyalew3@gmail.com>
2024-08-27 12:08:11 +0300

    book controller... add Book...

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,5 +1,23 @@
 export const addBook = (req, res) => {
-    
+    try {
+        if (!req.body.title,
+            !req.body.author,
+            !req.body.category,
+            !req.body.price,
+            !req.body.availableBooks,
+            !req.body.language,
+            !req.body.pages,
+            !req.body.publisher,
+            !req.body.year,
+            !req.body.description
+            ) {
+            return res.status(400).send({
+                message: 'Enter all required fields!...'
+            })
+        }
+    } catch (error) {
+        
+    }
 };
 export const getBooks = (req, res) => {
 
