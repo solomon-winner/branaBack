39e2415f8e786db7c14f0bdcc8eeec2d480a60b8
solomon-winner <solyalew3@gmail.com>
2024-08-28 15:36:16 +0300

    ...the update user controller...

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,5 +8,63 @@ export const getUsers = async (req, res) => {
     res.status(500).json({error: 'Internal Server Error!'})
 }
 }
-export const getUser = (req,res) => {}
-export const updateUser = (req,res) => {}
+export const getUser = (req,res) => {
+    try {
+        const {id} = req.params;
+    } catch (error) {
+        
+    }
+}
+export const updateUser = async (req,res) => {
+    try {
+        const {id} = req.params;
+    const {
+        firstName,
+        lastName,
+        email,
+        password,
+        role,
+        profilePicture,
+        wishList,
+        phoneNo,
+        altPhoneNo,
+        BankAccounts,
+        address,
+        shelve ,
+        savedBooks,
+        recommendedBooks,
+        Categories,
+        authors 
+    } = req.body;
+    const UpdatedData = {
+        firstName,
+        lastName,
+        email,
+        password,
+        role,
+        profilePicture,
+        wishList,
+        phoneNo,
+        altPhoneNo,
+        BankAccounts,
+        address,
+        shelve ,
+        savedBooks,
+        recommendedBooks,
+        Categories,
+        authors
+    }
+    Object.keys(UpdatedData).forEach(key => {
+        if (!UpdatedData[key]) {
+            delete UpdatedData[key];
+        }
+    });
+    const updatedUser = await User.findByIdAndUpdate(id, UpdatedData, {new: true});
+    if (!updatedUser) {
+        return res.status(404).send({error: 'User not found!'})
+    }
+    res.json(updatedUser);
+    } catch (error) {
+        
+    }
+}
