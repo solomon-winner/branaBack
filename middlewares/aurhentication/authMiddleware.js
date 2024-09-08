ce95429052825c25c20cf6ea6a8636d364074a7a
solomon-winner <solyalew3@gmail.com>
2024-09-08 14:25:27 +0300

    ...authentication middleware...

---
+++ middlewares/aurhentication/authMiddleware.js
@@ -0,0 +1,6 @@
+export const AuthMiddleware = async (req,res, next) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) {
+        return res.status(401).send({error: 'No token, Authorization denied!'});
+    }
+}
