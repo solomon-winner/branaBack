176f7e70bb60b8091aa9603e45f2efc15166da7b
solomon-winner <solyalew3@gmail.com>
2025-03-12 11:49:11 +0300

    ...rate limiters...

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,24 +1,35 @@
 import { UserDTOForUser } from '../DTOS/userDTO/userdtoForUser.dto.js';
 import { AuthService } from '../services/authentication/auth.service.js';
+import { generateToken } from '../services/authentication/tokenGenerater.service.js';
 import ResponseHelper from '../utils/responseHelper.js';
 
-export const login = (req, res, next) => {
+export const login = async (req, res, next) => {
     try {
         const { email, password } = req.body;
-        const user = AuthService.checkExistingUser(email);
+        
+        if (!email || !password) {
+            return ResponseHelper.error(res, 'Email and password are required', [], 400);
+          }
+
+        const user = await AuthService.checkExistingUser(email);
+        
         if (!user) {
-            return ResponseHelper.error(res, 'User not found', [], 404);
+            return ResponseHelper.error(res, 'Invalid Credentials!', [], 404);
         }
-        const isPasswordMatch = AuthService.comparePassword(password, user.password);
+
+        const isPasswordMatch = await AuthService.comparePassword(password, user.password);
+        
         if (!isPasswordMatch) {
             return ResponseHelper.error(res, 'Invalid password', [], 400);
         }
-        const token = AuthService.generateToken(user);
+
+        const token = generateToken(user);
         ResponseHelper.success(res, 'Login successful', { token, user: new UserDTOForUser(user) }, 200);
     } catch (error) {
         next(error);
     }
 }
+
 export const register = async (req, res, next) => {
     try {
         const { firstName, lastName, email, password, phoneNo, altPhoneNo } = req.body;
