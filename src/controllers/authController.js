a0a52f1313f11106b4b43451425c63026633ffa4
solomon-winner <solyalew3@gmail.com>
2025-03-12 11:24:26 +0300

    ...login controller...

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,7 +2,23 @@ import { UserDTOForUser } from '../DTOS/userDTO/userdtoForUser.dto.js';
 import { AuthService } from '../services/authentication/auth.service.js';
 import ResponseHelper from '../utils/responseHelper.js';
 
-export const login = (req, res, next) => {}
+export const login = (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = AuthService.checkExistingUser(email);
+        if (!user) {
+            return ResponseHelper.error(res, 'User not found', [], 404);
+        }
+        const isPasswordMatch = AuthService.comparePassword(password, user.password);
+        if (!isPasswordMatch) {
+            return ResponseHelper.error(res, 'Invalid password', [], 400);
+        }
+        const token = AuthService.generateToken(user);
+        ResponseHelper.success(res, 'Login successful', { token, user: new UserDTOForUser(user) }, 200);
+    } catch (error) {
+        next(error);
+    }
+}
 export const register = async (req, res, next) => {
     try {
         const { firstName, lastName, email, password, phoneNo, altPhoneNo } = req.body;
@@ -26,7 +42,7 @@ export const register = async (req, res, next) => {
         return ResponseHelper.error(res, 'User creation failed', [], 500);
     }
     
-    ResponseHelper.success(res, 'User Registered successfully', UserDTOForUser(user), 201);
+    ResponseHelper.success(res, 'User Registered successfully', new UserDTOForUser(user), 201);
 
     } catch (error) {
         next(error);
