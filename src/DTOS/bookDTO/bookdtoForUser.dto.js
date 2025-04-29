7721c5398985d409c30521f00f6e5b63e1980324
solomon-winner <solyalew3@gmail.com>
2025-04-29 15:50:13 +0300

    ...books dto for a user...

---
+++ src/DTOS/bookDTO/bookdtoForUser.dto.js
@@ -0,0 +1,26 @@
+export class BookDTOForUser {
+    constructor({ _id, title, author, img, rating, price, description, category, availableBooks, language, pages, publisher, year, isBestSeller, isTrending, isOnSale, isDiscounted, discount, discountedPrice, isComingSoon, isPreOrder, isSoldOut }) {
+        this.id = _id;
+        this.title = title;
+        this.author = author;
+        this.img = img;
+        this.rating = rating;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.availableBooks = availableBooks;
+        this.language = language;
+        this.pages = pages;
+        this.publisher = publisher;
+        this.year = year;
+        this.isBestSeller = isBestSeller;
+        this.isTrending = isTrending;
+        this.isOnSale = isOnSale;
+        this.isDiscounted = isDiscounted;
+        this.discount = discount;
+        this.discountedPrice = discountedPrice;
+        this.isComingSoon = isComingSoon;
+        this.isPreOrder = isPreOrder;
+        this.isSoldOut = isSoldOut;
+    }
+}
