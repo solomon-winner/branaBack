b770aa340847cda05f7ed70d612f5acd390d68ca
solomon-winner <solyalew3@gmail.com>
2025-05-01 16:12:40 +0300

    ...wish list service...

--- src/DTOS/favouriteDTO/book.dto.js
+++ src/DTOS/favouriteDTO/book.dto.js
@@ -1,10 +1,18 @@
 export class bookFavouriteDto {
-    constructor({ _id, img, title, price, author, reason }) {
-        this.bookId = _id;
-        this.img = img;
-        this.title = title;
-        this.price = price;
-        this.author = author;
-        this.reason = reason;
+    constructor(data, type = 'default') {
+      const book = data.bookId && typeof data.bookId === 'object' ? data.bookId : data;
+  
+      this.bookId = book._id || data._id || null;
+      this.title = book.title || null;
+      this.img = book.img || null;
+      this.author = book.author || null;
+  
+      if (type === 'recommended') {
+        this.reason = data.reason || null;
+      }
+      if (type === 'wishlist') {
+        this.price = data.price || null;
+      }
     }
-}
+  }
+  
--- src/services/WishList/wishList.service.js
+++ src/services/WishList/wishList.service.js
@@ -1,10 +1,12 @@
+import { bookFavouriteDto } from "../../DTOS/favouriteDTO/book.dto.js";
 import { UserCollections } from "../../models/userCollections.js";
 
 export const wishListService = {
     getWishList: async (userId) => {
         try {
-            const wishList = await UserCollections.find({ userId, collectionType:'wishlist' }).populate("bookId");
-            return wishList;
+            const wishList = await UserCollections.find({ userId, collectionType:'wishlist' }).populate("bookId", "title author img price").select("-__v").lean();
+             
+            return wishList.map((book) => new bookFavouriteDto(book));
         } catch (error) {
             throw new Error("Error fetching wish list");
         }
@@ -12,13 +14,13 @@ export const wishListService = {
 
     addWishList: async (userId, bookId, price) => {
         try {
-            const existingWish = await WishList.findOne({ userId, targetId: bookId, targetType: "Book", collectionType: "wishlist", price });
+            const existingWish = await UserCollections.findOne({ userId, targetId: bookId, collectionType: "wishlist" });
             if (existingWish) {
                 throw new Error("Book already in wish list");
             }
-            const newWish = new WishList({ userId, bookId });
+            const newWish = new WishList({ userId, bookId, targetId: bookId,targetType: "Book", collectionType: "wishlist", price });
             await newWish.save();
-            return newWish;
+            return new bookFavouriteDto(newWish);
         } catch (error) {
             throw new Error("Error adding to wish list");
         }
@@ -26,7 +28,7 @@ export const wishListService = {
 
     removeWishList: async (userId, bookId) => {
         try {
-            await WishList.deleteOne({ userId, bookId, collectionType: "wishlist" });
+            await UserCollections.deleteOne({ userId, bookId, collectionType: "wishlist" });
             return { message: "Book removed from wish list" };
         } catch (error) {
             throw new Error("Error removing from wish list");
