7282f90d91568fd235f13a7b9a7c0cb5fd646b15
solomon-winner <solyalew3@gmail.com>
2025-05-01 16:18:29 +0300

    ...wish list controller...

--- src/controllers/wishListController.js
+++ src/controllers/wishListController.js
@@ -1,4 +1,34 @@
-export const gtWishList = async (req, res) => {}
-export const addWishList = async (req, res) => {}
-export const removeWishList = async (req, res) => {}
+import { RecomendedBooksService } from "../services/RecommededBooks/recommendedBooks.service.js";
+import ResponseHelper from "../utils/responseHelper.js";
+
+export const gtWishList = async (req, res, next) => {
+    try {
+        const { userId } = req.params;
+        const wishList = await RecomendedBooksService.getWishList(userId);
+        return ResponseHelper.success(res, 'Wish list retrieved successfully', wishList, 200);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const addWishList = async (req, res, next) => {
+    try {
+        const { userId, bookId, price } = req.body;
+        const wishList = await RecomendedBooksService.addWishList(userId, bookId, price);
+        return ResponseHelper.success(res, 'Book added to wish list successfully', wishList, 201);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const removeWishList = async (req, res, next) => {
+    try {
+        const { userId } = req.params;
+        const { bookId } = req.body;
+        const removedBook = await RecomendedBooksService.removeWishList(userId, bookId);
+        return ResponseHelper.success(res, 'Book removed from wish list successfully', removedBook, 200);
+    } catch (error) {
+        next(error);
+    }
+}
 
