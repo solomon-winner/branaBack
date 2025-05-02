790eb01e32f576eec6860ebf08a96640d3b08d35
solomon-winner <solyalew3@gmail.com>
2025-05-02 15:27:11 +0300

    ...bank account service...

--- src/controllers/bankAccountController.js
+++ src/controllers/bankAccountController.js
@@ -1,4 +1,4 @@
-export const addAccount = (req, res) => {};
-export const updateAccount = (req, res) => {};
-export const getAccounts = (req, res) => {};
-export const removeAccount = (req, res) => {};
+export const addAccount = (req, res, next) => {};
+export const updateAccount = (req, res, next) => {};
+export const getAccounts = (req, res, next) => {};
+export const removeAccount = (req, res, next) => {};
