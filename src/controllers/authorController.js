bf6546f341b43bcf545da42878ff7eabd08d0657
solomon-winner <solyalew3@gmail.com>
2025-05-07 21:05:39 +0300

    ...authors controller...

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,5 +1,31 @@
-export const addAuthors = (req, res, next) => {};
-export const getAuthors = (req, res, next) => {};
-export const getAuthorById = (req, res, next) => {};
-export const deleteAuthor = (req, res, next) => {};
-export const updateAuthors = (req, res, next) => {}
+import { AuthorService } from '../services/Author/author.service.js';
+import ResponseHelper from '../utils/responseHelper.js';
+
+export const addAuthors = async (req, res, next) => {
+    try {
+        const { name, img, bio, birthDate, deathDate } = req.body;
+        const authorData = {
+            name,
+            img,
+            bio,
+            birthDate,
+            deathDate
+        };
+        const newAuthor = await AuthorService.addAuthors(authorData);
+        return ResponseHelper.success(res, 'Author added successfully', newAuthor, 201);
+    } catch (error) {
+        next(error);
+    }
+};
+export const getAuthors = async (req, res, next) => {
+    try {
+        const {page, limit} = req.query;
+        const authors = await AuthorService.getAuthors({page, limit});
+        return ResponseHelper.success(res, 'Authors fetched successfully!', authors, 200);
+    } catch (error) {
+        next(error);
+    }
+};
+export const getAuthorById = async (req, res, next) => {};
+export const deleteAuthor = async (req, res, next) => {};
+export const updateAuthors = async (req, res, next) => {}
