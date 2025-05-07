d3d975058fb9e67fab6e57c85f3ec2c98573d144
solomon-winner <solyalew3@gmail.com>
2025-05-07 23:51:53 +0300

    ...shelve servie...

---
+++ src/services/Shelve/shelve .service.js
@@ -0,0 +1,52 @@
+import { Shelve } from "../../models/shelve";
+
+export const ShelveService = {
+    addShelve: async (shelveData) => {
+        try {
+            const shelve = new Shelve(shelveData);
+            await shelve.save();
+            return new ShelveDTOForUser(shelve);
+        } catch (error) {
+            console.error('Error adding shelve:', error);
+            throw new Error('Error adding shelve: ' + error.message);
+        }
+    },
+
+    getShelves: async (userId) => {
+        try {
+            const shelves = await Shelve.find({ user: userId }).populate('book').select('-__v -createdAt -updatedAt');
+            if (!shelves) {
+                throw new Error('Shelves not found');
+            }
+            return shelves.map((shelve) => new ShelveDTOForUser(shelve));
+        } catch (error) {
+            console.error('Error fetching shelves:', error);
+            throw new Error('Error fetching shelves: ' + error.message);
+        }
+    },
+    removeFromShelve: async (shelveId) => {
+        try {
+            const shelve = await Shelve.findByIdAndDelete(shelveId);
+            if (!shelve) {
+                throw new Error('Shelve not found');
+            }
+            return shelve;
+        } catch (error) {
+            console.error('Error removing from shelve:', error);
+            throw new Error('Error removing from shelve: ' + error.message);
+        }
+    },
+    removeWholeShelve: async (userId) => {
+        try {
+            const shelve = await Shelve.deleteMany({ user: userId });
+            if (!shelve) {
+                throw new Error('Shelve not found');
+            }
+            return shelve;
+        } catch (error) {
+            console.error('Error removing from shelve:', error);
+            throw new Error('Error removing from shelve: ' + error.message);
+        }
+    }
+}
+
