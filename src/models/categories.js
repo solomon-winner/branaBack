7bc6fa6b5f49e5f3fec1f5cdf488b73bb790a077
solomon-winner <solyalew3@gmail.com>
2025-05-08 15:55:03 +0300

    ...category services...

--- src/models/categories.js
+++ src/models/categories.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema({
     name: {
+        unique: true,
+        trim: true,
         type: String,
         required: true,
     },
--- src/services/Category/category.service.js
+++ src/services/Category/category.service.js
@@ -12,8 +12,50 @@ export const CategoryService = {
                  throw new Error('Error fetching categories: ' + error.message);
             }
      },
-     getCategoryById: async(id) => {},
-     createCategory: async(categoryData) =>{},
-     updateCategory: async(id, categoryData) =>{},
-     deleteCategory: async(id) =>{},
+     getCategoryById: async(id) => {
+        try {
+            const category = await Category.findById(id).select('-__v -createdAt -updatedAt').lean();
+            if (!category) {
+                throw new Error('Category not found');
+            }
+            return category;
+        } catch (error) {
+            console.error('Error fetching category:', error);
+            throw new Error('Error fetching category: ' + error.message);
+            
+        }
+     },
+     createCategory: async(categoryData) =>{
+        try {
+            const newCategory = await Category.create(categoryData);
+            return newCategory;
+        } catch (error) {
+            console.error('Error creating category:', error);
+            throw new Error('Error creating category: ' + error.message);
+        }
+     },
+     updateCategory: async(id, categoryData) =>{
+        try {
+            const updatedCategory = await Category.findByIdAndUpdate(id, categoryData, { new: true }).select('-__v -createdAt -updatedAt').lean();
+            if (!updatedCategory) {
+                throw new Error('Category not found');
+            }
+            return updatedCategory;
+        } catch (error) {
+            console.error('Error updating category:', error);
+            throw new Error('Error updating category: ' + error.message);
+        }
+     },
+     deleteCategory: async(id) =>{
+        try {
+            const deletedCategory = await Category.findByIdAndDelete(id).select('-__v -createdAt -updatedAt').lean();
+            if (!deletedCategory) {
+                throw new Error('Category not found');
+            }
+            return deletedCategory;
+        } catch (error) {
+            console.error('Error deleting category:', error);
+            throw new Error('Error deleting category: ' + error.message);
+        }
+     },
 }
