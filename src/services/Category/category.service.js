9e6f987064f6a28c397f31d7140a799d4e8f966c
solomon-winner <solyalew3@gmail.com>
2025-05-08 14:13:46 +0300

    ...sending the categories in a page with meta data...

--- src/services/Category/category.service.js
+++ src/services/Category/category.service.js
@@ -1,10 +1,12 @@
 import { Category } from "../../models/categories";
+import getPagination from "../../utils/getPagination";
 
 export const CategoryService = {
      getAllCategories: async(page, limit) => {
             try {
+                const metaData = await getPagination(page, limit, Category);
                  const categories = await Category.find({}).select('-__v -createdAt -updatedAt').skip((page - 1) * limit).limit(limit).lean();
-                 return categories;
+                 return {metaData, categories};
             } catch (error) {
                  console.error('Error fetching categories:', error);
                  throw new Error('Error fetching categories: ' + error.message);
