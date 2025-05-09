e2c5d7f28160212f33a9568c7de1c97182c8a543
solomon-winner <solyalew3@gmail.com>
2025-05-09 13:20:32 +0300

    ...

--- src/DTOS/favouriteDTO/book.dto.js
+++ src/DTOS/favouriteDTO/book.dto.js
@@ -1,16 +1,16 @@
 export class bookFavouriteDto {
     constructor(data, type = 'default') {
-      const book = data.bookId && typeof data.bookId === 'object' ? data.bookId : data;
+      const book = data.targetId && typeof data.targetId === 'object' ? data.targetId : data;
   
       this.bookId = book._id || data._id || null;
       this.title = book.title || null;
       this.img = book.img || null;
       this.author = book.author || null;
   
-      if (type === 'recommended') {
+      if (data.collectionType === 'recommended') {
         this.reason = data.reason || null;
       }
-      if (type === 'wishlist') {
+      if (data.collectionType === 'wishlist') {
         this.price = data.price || null;
       }
     }
--- src/services/WishList/wishList.service.js
+++ src/services/WishList/wishList.service.js
@@ -5,11 +5,11 @@ import { UserCollections } from "../../models/userCollections.js";
 export const wishListService = {
     getWishList: async (userId) => {
         try {
-            const wishList = await UserCollections.find({ userId, collectionType:'wishlist' }).populate("bookId", "title author img price").select("-__v").lean();
-             
+            const wishList = await UserCollections.find({ userId, collectionType:'wishlist' }).populate("targetId", "title author img price").select("-__v").lean();
+            console.log("Wish list found:", wishList);
             return wishList.map((book) => new bookFavouriteDto(book));
         } catch (error) {
-            throw new Error("Error fetching wish list");
+            throw new Error("Error fetching wish list:"+ error.message);
         }
     },
 
@@ -20,14 +20,15 @@ export const wishListService = {
             if (!book) {
                 throw new Error("Book not found");
             }
-
             const newWish = new UserCollections({ userId, bookId, targetId: bookId, targetType: "Book", collectionType: "wishlist", price: book.price });
             await newWish.save();
+            console.log("Book found:", newWish);
             return new bookFavouriteDto(newWish);
         } catch (error) {
             if (error.code === 11000) {
             throw new Error("Book already in wish list");
         }
+    
             console.error(error);
             throw new Error("Error adding to wish list");
         }
