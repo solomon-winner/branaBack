eff3f2613663edf6b2ccc959e0c992f2c891ac76
solomon-winner <solyalew3@gmail.com>
2025-06-01 17:06:31 +0300

    ...creating validation for add shelve endpoint...

---
+++ src/validation/shelve/shelveValidation.js
@@ -0,0 +1,33 @@
+import { validate } from 'express-validation';
+import Joi from 'joi';
+
+const objectId = Joi.string().hex().length(24).required().messages({
+  'string.length': 'ID must be 24 characters long',
+  'string.hex': 'ID must be a valid ObjectId',
+  'any.required': '{#label} is required',
+});
+
+const createShelveSchema = {
+  body: Joi.object({
+    user: objectId.label('User ID'),
+    book: objectId.label('Book ID'),
+    bookCount: Joi.number().integer().min(1).default(1).messages({
+      'number.base': 'Book count must be a number',
+      'number.integer': 'Book count must be an integer',
+      'number.min': 'Book count must be at least 1',
+    }),
+    price: Joi.number().min(0).required().messages({
+      'number.base': 'Price must be a number',
+      'number.min': 'Price must be a non-negative number',
+      'any.required': 'Price is required',
+    }),
+    to: Joi.string().trim().default('me').messages({
+      'string.base': '"to" must be a string',
+    }),
+    isPaied: Joi.boolean().default(false).messages({
+      'boolean.base': 'isPaied must be a boolean value',
+    }),
+  }).required(),
+};
+
+export const validateCreateShelve = validate(createShelveSchema, {}, {});
